9ab05aa9cbbe59410c3c82d616ca527fa06bdc50
tom-1313 <trgadacy@quinnipiac.edu>
2021-03-24 11:03:05 -0400

    Commented some ideas, copied route method over

--- DistanceVectorRouter.java
+++ DistanceVectorRouter.java
@@ -7,6 +7,10 @@
 import java.util.ArrayList;
 
 public class DistanceVectorRouter extends Router {
+    /*
+    TODO: 1. Create a Table for the neighboring Routers based on the fastest times from the ping. 2. Create a thread that updates the table every so often
+     */
+
     // A generator for the given DistanceVectorRouter class
     public static class Generator extends Router.Generator {
         public Router createRouter(int id, NetworkInterface nic) {
@@ -28,6 +32,7 @@ public class DistanceVectorRouter extends Router {
             NetworkInterface.TransmitPair toSend = nic.getTransmit();
             if (toSend != null) {
                 // There is something to send out
+                // Check the table to see what route is the fastest to the destination, send it to that router
                 process = true;
                 debug.println(3, "(DistanceVectorRouter.run): I am being asked to transmit: " + toSend.data + " to the destination: " + toSend.destination);
             }
@@ -35,6 +40,8 @@ public class DistanceVectorRouter extends Router {
             NetworkInterface.ReceivePair toRoute = nic.getReceived();
             if (toRoute != null) {
                 // There is something to route through - or it might have arrived at destination
+                // if the destination = this router, do nothing
+                // else send it to the fastest router based on the table
                 process = true;
                 debug.println(3, "(DistanceVectorRouter.run): I am being asked to transmit: " + toSend.data + " to the destination: " + toSend.destination);
             }
@@ -44,5 +51,20 @@ public class DistanceVectorRouter extends Router {
                 try { Thread.sleep(1); } catch (InterruptedException e) { }
             }
         }
+    }
+
+   //not implemented yet
+    /** Route the given packet out.
+     In our case, we go to all nodes except the originator
+     **/
+    private void route(int linkOriginator, FloodRouter.Packet p) {
+//        ArrayList<Integer> outLinks = nic.getOutgoingLinks();
+//        int size = outLinks.size();
+//        for (int i = 0; i < size; i++) {
+//            if (outLinks.get(i) != linkOriginator) {
+//                // Not the originator of this packet - so send it along!
+//                nic.sendOnLink(i, p);
+//            }
+//        }
     }
 }
